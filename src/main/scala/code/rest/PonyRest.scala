acaff393f5ffb63f16843f2c6b4b0a40d37985b4
Doug Roper <roperdj@gmail.com>
2013-10-02 18:04:25 -0400

    Pulled in PonyRest

--- src/main/scala/code/rest/PonyRest.scala
+++ src/main/scala/code/rest/PonyRest.scala
@@ -1,11 +1,13 @@
 package code.rest
 
 import net.liftweb.http.rest.RestHelper
-import net.liftweb.http.{GetRequest, Req}
-import code.model.{Pony, PonyService}
+import net.liftweb.http._
+import code.model.PonyService
 import net.liftweb.json.Extraction
-import net.liftweb.common.{Box, Failure, Full}
 
 class PonyRest extends RestHelper {
-
+  serve {
+    case Req("rest" :: "pony" :: "best" :: Nil, _, GetRequest) =>
+      PonyService.getRandomPony.map(pony => Extraction.decompose(pony))
+  }
 }
